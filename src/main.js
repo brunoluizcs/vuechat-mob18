db8d1822e48e22419f150d7e4f372c2bcbd637ac
Bruno Santos <bruno.santos@encripta.com.br>
2019-12-13 13:12:46 -0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import VueChatScroll from 'vue-chat-scroll'
 import App from './App.vue'
+import router from './router'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
+Vue.use(VueChatScroll)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
