94e736d7fb006755d9b79ea0b409cb65f8b67dec
Bruno Santos <bruno.santos@encripta.com.br>
2019-12-16 15:46:53 -0300

    incluindo plugin vue no eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,7 @@ module.exports = {
     parser : "babel-eslint",
     rules: {
         'no-console': 'off'   
-    }
+    },
+    extends: ["vue", "standard", "plugin:vue/recommended"],
+    plugins: ["import", "vue"]
 }
