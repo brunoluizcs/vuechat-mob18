f42502032b47844b9d890c5751eca93eee991bfb
Bruno Santos <bruno.santos@encripta.com.br>
2019-12-16 16:50:38 -0300

    removendo informações do .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-    parser : "babel-eslint",
     rules: {
         'no-console': 'off'   
-    },
-    extends: ["vue", "standard", "plugin:vue/recommended"],
-    plugins: ["import", "vue"]
+    }
 }
