359af9282f783711302651db1f0f3a1cd58d073e
Bruno Santos <bruno.santos@encripta.com.br>
2019-12-17 11:26:19 -0300

    Realizando mudanças para deploy

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    parser : "babel-eslint",
-    rules: {
-        'no-console': 'off'   
-    },
-    extends: ["vue", "standard", "plugin:vue/recommended"],
-    plugins: ["import", "vue"]
-}
